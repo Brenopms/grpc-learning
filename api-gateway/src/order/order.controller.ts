aa483b4fd249fe0d44c0a4eaff826dfd479a0873
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-02 15:23:31 -0300

    feat(api-gateway): add order controller and register order module

--- api-gateway/src/order/order.controller.ts
+++ api-gateway/src/order/order.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, OnModuleInit, Post, Req, UseGuards } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { RequestModel } from 'src/common/requestModel';
+import { CreateOrderRequest, CreateOrderResponse, OrderServiceClient, ORDER_SERVICE_NAME } from './order.pb';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController implements OnModuleInit {
+  private orderServiceClient: OrderServiceClient;
+
+  @Inject(ORDER_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.orderServiceClient = this.client.getClientByServiceName(ORDER_SERVICE_NAME);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  private async createOrder(@Req() req: RequestModel): Promise<Observable<CreateOrderResponse>> {
+    const body: CreateOrderRequest = req.body;
+
+    body.userId = req.user;
+
+    return this.orderServiceClient.createOrder(body);
+  }
+}
