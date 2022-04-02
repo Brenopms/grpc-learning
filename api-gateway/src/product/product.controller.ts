c8c4d6f4b8be8b5d79ff2a6cfad76b8908c85b23
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-02 15:38:10 -0300

    feat(api-gateway): create product controller and module

--- api-gateway/src/product/product.controller.ts
+++ api-gateway/src/product/product.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Inject, OnModuleInit, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import { AuthGuard } from 'src/auth/auth.guard';
+import {
+  CreateProductRequest,
+  CreateProductResponse,
+  FindOneResponse,
+  ProductServiceClient,
+  PRODUCT_SERVICE_NAME,
+} from './product.pb';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController implements OnModuleInit {
+  private productServiceClient: ProductServiceClient;
+
+  @Inject(PRODUCT_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.productServiceClient = this.client.getClientByServiceName<ProductServiceClient>(PRODUCT_SERVICE_NAME);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  private async createProduct(@Body() body: CreateProductRequest): Promise<Observable<CreateProductResponse>> {
+    return this.productServiceClient.createProduct(body);
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard)
+  private async findOne(@Param('id', ParseIntPipe) id: number): Promise<Observable<FindOneResponse>> {
+    return this.productServiceClient.findOne({ id });
+  }
+}
--- api-gateway/src/product/product.module.ts
+++ api-gateway/src/product/product.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ORDER_PACKAGE_NAME, ORDER_SERVICE_NAME } from 'src/order/order.pb';
 import { ProductController } from './product.controller';
 
 @Module({
-  controllers: [ProductController]
+  imports: [
+    ClientsModule.register([
+      {
+        name: ORDER_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50053',
+          package: ORDER_PACKAGE_NAME,
+          protoPath: '../grpc-proto/proto/product.proto',
+        },
+      },
+    ]),
+  ],
+  controllers: [ProductController],
 })
 export class ProductModule {}
