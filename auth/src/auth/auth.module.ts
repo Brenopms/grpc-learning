0463097783ad87e6d118e55c88cf252c0001ad0b
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-03 19:08:24 -0300

    feat(auth): configure auth modules

--- auth/src/auth/auth.module.ts
+++ auth/src/auth/auth.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { UserRepository } from './repository/user.repository';
+import { AuthService } from './service/auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { JwtService } from './service/jwt.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  controllers: [AuthController]
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtService, JwtStrategy, UserRepository, PrismaService],
 })
 export class AuthModule {}
