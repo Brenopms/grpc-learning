4850e5aee4cd77d13368e910b3abd5e616c03837
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-03 14:22:08 -0300

    refactor: update auth dto to use user entity

--- auth/src/auth/auth.dto.ts
+++ auth/src/auth/auth.dto.ts
@@ -1,28 +1,29 @@
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import { User } from './auth.entity';
 import { LoginRequest, RegisterRequest, ValidateRequest } from './auth.pb';
 
 export class LoginRequestDto implements LoginRequest {
   @IsEmail()
   @MaxLength(50)
-  public readonly username: string;
+  public readonly username: User['username'];
 
   @IsString()
   @MaxLength(100)
-  public readonly password: string;
+  public readonly password: User['password'];
 }
 
 export class RegisterRequestDto implements RegisterRequest {
   @IsEmail()
-  public readonly email: string;
+  public readonly email: User['email'];
 
   @IsString()
   @MinLength(8)
   @MaxLength(100)
-  public readonly password: string;
+  public readonly password: User['password'];
 
   @IsString()
   @MaxLength(50)
-  public readonly username: string;
+  public readonly username: User['username'];
 }
 
 export class ValidateRequestDto implements ValidateRequest {
