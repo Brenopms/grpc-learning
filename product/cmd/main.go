2067b1a49fc44a8bcd4f6d5698d5c0cd777ed755
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-08 00:24:06 -0300

    feat(product): add main package

---
+++ product/cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/Brenopms/grpc-learning/pkg/config"
+	"github.com/Brenopms/grpc-learning/pkg/db"
+	"github.com/Brenopms/grpc-learning/pkg/pb"
+	"github.com/Brenopms/grpc-learning/pkg/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	envConfig, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Could not load environment configuration")
+	}
+
+	dbHandler := db.Init(envConfig.DBUrl)
+
+	netListener, err := net.Listen("tcp", envConfig.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to Start TCP Server")
+	}
+
+	fmt.Println("Product Service running on port: ", envConfig.Port)
+
+	server := services.Server{
+		DbHandler: dbHandler,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterProductServiceServer(grpcServer, &server)
+
+	if err := grpcServer.Serve(netListener); err != nil {
+		log.Fatalln("Failed to Run grpc server")
+	}
+
+}
