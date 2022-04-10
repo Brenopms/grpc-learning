9212f709a2d2c536f2e2d253f460a39799b50200
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-10 15:02:16 -0300

    refactor(product): add gorm tags for not null fields

--- product/pkg/models/product.go
+++ product/pkg/models/product.go
@@ -1,10 +1,10 @@
 package models
 
 type Product struct {
-	Id                int32            `json:"id" gorm:"primaryKey" validate:"required"`
-	Name              string           `json:"name" validate:"required"`
-	Stock             int32            `json:"stock" validate:"required,gte=0"`
-	Price             float64          `json:"price" validate:"required,gte=0"`
-	Sku               string           `json:"sku" validate:"required"`
+	Id                int32            `json:"id" gorm:"primaryKey"`
+	Name              string           `json:"name" gorm:"not null" validate:"required"`
+	Stock             int32            `json:"stock" gorm:"not null" validate:"required,gte=0"`
+	Price             float64          `json:"price" gorm:"not null" validate:"required,gte=0"`
+	Sku               string           `json:"sku" gorm:"not null" validate:"required"`
 	StockDecreaseLogs StockDecreaseLog `gorm:"foreignKey:ProductId"`
 }
--- product/pkg/models/stock_decrease_log.go
+++ product/pkg/models/stock_decrease_log.go
@@ -2,6 +2,6 @@ package models
 
 type StockDecreaseLog struct {
 	Id        int32 `json:"id" gorm:"primaryKey"`
-	OrderId   int32 `json:"order_id"`
-	ProductId int32 `json:"product_id"`
+	OrderId   int32 `json:"order_id" gorm:"not null"`
+	ProductId int32 `json:"product_id" gorm:"not null"`
 }
