ee690052f166cd735babb8145179f08907ccc100
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-11 23:14:00 -0300

    fix(order): add quantity on missing structs

--- order/pkg/models/order.go
+++ order/pkg/models/order.go
@@ -3,6 +3,7 @@ package models
 type Order struct {
 	Id        int32   `json:"id" gorm:"primaryKey"`
 	Price     float64 `json:"price" gorm:"not null" validate:"required,gte=0"`
+	Quantity  int32   `json:"quantity" gorm:"not null" validate:"required,gte=0"`
 	ProductId int32   `json:"product_id" gorm:"not null" validate:"required"`
 	UserId    string  `json:"user_id" gorm:"not null" validate:"required"`
 }
--- order/pkg/services/order.go
+++ order/pkg/services/order.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"log"
 	"net/http"
 
 	"github.com/Brenopms/grpc-learning/order/pkg/client"
@@ -40,6 +41,14 @@ func (server *Server) CreateOrder(ctx context.Context, req *pb.CreateOrderReques
 	}
 	product, err := server.ProductServiceClient.FindOne(req.ProductId)
 
+	if err != nil {
+		return &pb.CreateOrderResponse{Status: http.StatusBadRequest, Error: []string{err.Error()}}, nil
+	} else if product.Status == http.StatusNotFound {
+		return &pb.CreateOrderResponse{Status: product.Status, Error: product.Error}, nil
+	} else if product.Data.Stock < req.Quantity {
+		return &pb.CreateOrderResponse{Status: http.StatusConflict, Error: []string{"Not enough stock for the requested product"}}, nil
+	}
+
 	order := models.Order{
 		Price:     calculateOrderPrice(product.Data.Price, req.Quantity),
 		ProductId: product.Data.Id,
@@ -59,25 +68,18 @@ func (server *Server) CreateOrder(ctx context.Context, req *pb.CreateOrderReques
 		}, nil
 	}
 
-	if err != nil {
-		return &pb.CreateOrderResponse{Status: http.StatusBadRequest, Error: []string{err.Error()}}, nil
-	} else if product.Status == http.StatusNotFound {
-		return &pb.CreateOrderResponse{Status: product.Status, Error: product.Error}, nil
-	} else if product.Data.Stock < req.Quantity {
-		return &pb.CreateOrderResponse{Status: http.StatusConflict, Error: []string{"Not enough stock for the requested product"}}, nil
-	}
-
 	server.DbHandler.DB.Create(&order)
 
-	res, err := server.ProductServiceClient.DecreaseStock(req.ProductId, order.Id)
+	res, err := server.ProductServiceClient.DecreaseStock(req.ProductId, order.Id, order.Quantity)
+	log.Println(err, res)
 
 	if err != nil {
 		return &pb.CreateOrderResponse{Status: http.StatusBadRequest, Error: []string{err.Error()}}, nil
-	} else if res.Status == http.StatusConflict {
+	} else if res.Status == http.StatusConflict || res.Status == http.StatusBadRequest {
 		// Order was already placed but not computed
 		server.DbHandler.DB.Delete(&models.Order{}, order.Id)
 
-		return &pb.CreateOrderResponse{Status: http.StatusConflict, Error: res.Error}, nil
+		return &pb.CreateOrderResponse{Status: res.Status, Error: res.Error}, nil
 	}
 
 	return &pb.CreateOrderResponse{
