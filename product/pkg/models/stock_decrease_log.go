ca471ade3655fa36c8f22f90776187903a0da964
Breno Magalhães <breno.magalhaes@vallourec.com>
2022-04-11 22:08:54 -0300

    feat: add quantity to decreaseStock request

--- product/pkg/models/stock_decrease_log.go
+++ product/pkg/models/stock_decrease_log.go
@@ -4,4 +4,5 @@ type StockDecreaseLog struct {
 	Id        int32 `json:"id" gorm:"primaryKey"`
 	OrderId   int32 `json:"order_id" gorm:"not null"`
 	ProductId int32 `json:"product_id" gorm:"not null"`
+	Quantity  int32 `json:"quantity" gorm:"not null"`
 }
